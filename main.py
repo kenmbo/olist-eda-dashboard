b28de07646c155a10b062ba53ec9603a3fd077b7
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-03-08 21:29:25 -0500

    Finished adding the package imports. Adding the SQL queries.

--- main.py
+++ main.py
@@ -5,3 +5,396 @@ import sqlite3
 import seaborn as sns
 import numpy as np
 import plotly
+import squarify
+import statsmodels
+import folium
+import dash
+
+conn = sqlite3.connect('olist.sqlite') # SQL server connect to the database (local file)
+
+orders_per_day = """
+SELECT
+    DATE(order_purchase_timestamp) AS day,
+    COUNT(*) AS order_count
+FROM orders
+GROUP BY day
+"""
+
+order_day_hour = """
+SELECT
+    -- Day of the week abreviated
+    CASE STRFTIME('%w', order_purchase_timestamp)
+        WHEN '1' THEN 'Mon'
+        WHEN '2' THEN 'Tue'
+        WHEN '3' THEN 'Wed'
+        WHEN '4' THEN 'Thu'
+        WHEN '5' THEN 'Fri'
+        WHEN '6' THEN 'Sat'
+        WHEN '0' THEN 'Sun'
+        END AS day_of_week_name,
+    -- Day of the week as integer (Sunday=7)
+    CAST(STRFTIME('%w', order_purchase_timestamp) AS INTEGER) AS day_of_week_int,
+    -- Hour of the day (0-24)
+    CAST(STRFTIME("%H", order_purchase_timestamp) AS INTEGER) AS hour
+FROM orders
+"""
+
+count_orders_per_hour = ',\n    '.join([
+    f'COUNT(CASE WHEN hour = {i} THEN 1 END) AS "{i}"' \
+    for i in range(24)
+])
+
+orders_per_day_of_the_week_and_hour = f"""
+WITH OrderDayHour AS (
+    {order_day_hour}
+)
+SELECT
+    day_of_week_name,
+    {count_orders_per_hour}
+FROM OrderDayHour
+GROUP BY day_of_week_int
+ORDER BY day_of_week_int
+"""
+
+orders_per_city = """
+SELECT
+    customer_city AS customer_city,
+    UPPER(customer_city) AS city,
+    COUNT(orders.order_id) as city_order_count
+FROM
+    customers
+    JOIN orders USING (customer_id)
+GROUP BY customer_city
+ORDER BY city_order_count DESC
+LIMIT 10
+"""
+
+orders_per_city_reversed = f"""
+SELECT *
+FROM ({orders_per_city})
+ORDER BY city_order_count
+"""
+
+order_price_stats = """
+SELECT
+    MIN(order_price) AS min_order_price,
+    ROUND(AVG(order_price), 2) AS avg_order_price,
+    MAX(order_price) AS max_order_price
+FROM (
+    SELECT
+        orders.order_id,
+        SUM(order_items.price + order_items.freight_value) AS order_price
+    FROM orders
+        JOIN order_items USING (order_id)
+    GROUP BY orders.order_id
+)
+"""
+
+order_product_and_shipping_costs = """
+SELECT
+    orders.order_id,
+    SUM(price) AS product_cost,
+    SUM(freight_value) AS shipping_cost
+FROM
+    orders
+    JOIN order_items USING (order_id)
+WHERE order_status = 'delivered'
+GROUP BY orders.order_id
+"""
+
+ranked_categories = """
+SELECT
+    product_category_name_english AS category,
+    SUM(price) AS sales,
+    RANK() OVER (ORDER BY SUM(price) DESC) AS rank
+FROM order_items
+    JOIN orders USING (order_id)
+    JOIN products USING (product_id)
+    JOIN product_category_name_translation USING (product_category_name)
+WHERE order_status = 'delivered'
+GROUP BY product_category_name_english
+"""
+
+category_sales_summary = f"""
+WITH RankedCategories AS (
+    {ranked_categories}
+)
+
+SELECT
+    category,
+    sales
+FROM RankedCategories
+WHERE rank <= 18
+-- Other categories, aggregated
+UNION ALL
+SELECT
+    'Other categories' AS category,
+    SUM(sales) AS sales
+FROM RankedCategories
+WHERE rank > 18
+"""
+
+
+ordered_categories = f"""
+SELECT
+    product_weight_g AS weight,
+    product_category_name_english AS category,
+    ROW_NUMBER() OVER(PARTITION BY product_category_name_english ORDER BY product_weight_g)
+        AS category_row_n,
+    COUNT(*) OVER(PARTITION BY product_category_name_english) AS category_count
+FROM
+    products
+    JOIN order_items USING (product_id)
+    JOIN product_category_name_translation USING (product_category_name)
+WHERE
+    product_category_name_english IN {top_18_categories}
+"""
+
+ategories_by_median = f"""
+WITH OrderedCategories AS (
+    {ordered_categories}
+)
+SELECT category
+FROM OrderedCategories
+WHERE
+    -- Odd number of products: Select the middle row
+    (category_count % 2 = 1 AND category_row_n = (category_count + 1) / 2) OR
+    -- Even number of products: Select the two middle rows to be averaged
+    (category_count % 2 = 0 AND category_row_n IN ((category_count / 2), (category_count / 2 + 1)))
+GROUP BY category
+ORDER BY AVG(weight)
+"""
+
+selected_categories = ('health_beauty', 'auto', 'toys', 'electronics', 'fashion_shoes')
+
+monthly_sales_selected_categories = f"""
+SELECT
+    strftime('%Y-%m', order_purchase_timestamp) AS year_month,
+    SUM(CASE WHEN product_category_name_english = 'health_beauty' THEN price END) AS health_beauty,
+    SUM(CASE WHEN product_category_name_english = 'auto' THEN price END) AS auto,
+    SUM(CASE WHEN product_category_name_english = 'toys' THEN price END) AS toys,
+    SUM(CASE WHEN product_category_name_english = 'electronics' THEN price END) AS electronics,
+    SUM(CASE WHEN product_category_name_english = 'fashion_shoes' THEN price END) AS fashion_shoes
+FROM orders
+    JOIN order_items USING (order_id)
+    JOIN products USING (product_id)
+    JOIN product_category_name_translation USING (product_category_name)
+WHERE order_purchase_timestamp >= '2017-01-01'
+    AND product_category_name_english IN {selected_categories}
+GROUP BY year_month
+"""
+
+daily_sales_per_category = f"""
+SELECT
+    DATE(order_purchase_timestamp) AS date,
+    -- Days since 2017-01-01
+    CAST(JULIANDAY(order_purchase_timestamp) - JULIANDAY('2017-01-01') AS INTEGER) AS day,
+    product_category_name_english AS category,
+    SUM(price) AS sales
+FROM
+    orders
+    JOIN order_items USING (order_id)
+    JOIN products USING (product_id)
+    JOIN product_category_name_translation USING (product_category_name)
+WHERE
+    order_purchase_timestamp BETWEEN '2017-01-01' AND '2018-08-29'
+    AND category IN {selected_categories}
+GROUP BY
+    day,
+    product_category_name_english
+"""
+
+lm_per_category = f"""
+WITH DailySalesPerCategory AS (
+    {daily_sales_per_category}
+)
+SELECT
+    category,
+    -- Slope
+    (COUNT(*) * SUM(day * sales) - SUM(day) * SUM(sales)) /
+        (COUNT(*) * SUM(day * day) - SUM(day) * SUM(day))
+        AS slope,
+    -- Intercept
+    (SUM(sales) -
+        ((COUNT(*) * SUM(day * sales) - SUM(day) * SUM(sales)) /
+        (COUNT(*) * SUM(day * day) - SUM(day) * SUM(day))) *
+        SUM(day)) / COUNT(*)
+        AS intercept
+FROM
+    DailySalesPerCategory
+GROUP BY
+    category
+"""
+
+forecasted_sales_dec_2018 = f"""
+WITH DailySalesPerCategory AS (
+    {daily_sales_per_category}
+),
+LmPerCategory AS (
+    {lm_per_category}
+),
+ForecastedSales AS (
+    SELECT
+        DATE(date, '+1 year') AS date,
+        category,
+        -- Increase in predicted sales * sales 1 year ago
+        (intercept + slope * (day + CAST(JULIANDAY('2018-12-31') - JULIANDAY('2017-12-31') AS INTEGER)))
+            / (intercept + slope * day) * sales
+            AS forecasted_sales
+    FROM DailySalesPerCategory
+        JOIN LmPerCategory USING (category)
+    -- Filter for days of December 2018
+    WHERE day + CAST(JULIANDAY('2018-12-31') - JULIANDAY('2017-12-31') AS INTEGER)
+        BETWEEN CAST(JULIANDAY('2018-12-01') - JULIANDAY('2017-01-01') AS INTEGER)
+        AND CAST(JULIANDAY('2018-12-31') - JULIANDAY('2017-01-01') AS INTEGER)
+)
+SELECT
+    CAST(strftime('%d', date) AS INTEGER) AS december_2018_day,
+    category,
+    -- 5-day moving average
+    AVG(forecasted_sales)
+        OVER (PARTITION BY category ORDER BY date ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING)
+        AS moving_avg_sales
+FROM ForecastedSales
+"""
+
+order_stage_times_top_10_citites = f"""
+SELECT
+    UPPER(customer_city)
+        AS city,
+    AVG(JULIANDAY(order_approved_at) - JULIANDAY(order_purchase_timestamp))
+        AS approved,
+    AVG(JULIANDAY(order_delivered_carrier_date) - JULIANDAY(order_approved_at))
+        AS delivered_to_carrier,
+    AVG(JULIANDAY(order_delivered_customer_date) - JULIANDAY(order_delivered_carrier_date))
+        AS delivered_to_customer,
+    AVG(JULIANDAY(order_estimated_delivery_date) - JULIANDAY(order_delivered_customer_date))
+        AS estimated_delivery
+FROM orders
+    JOIN customers USING (customer_id)
+WHERE  customer_city IN {tuple(top_cities['customer_city'])}
+GROUP BY  customer_city
+ORDER BY approved + delivered_to_carrier + delivered_to_customer DESC
+"""
+
+daily_avg_shipping_time = """
+SELECT
+    DATE(order_purchase_timestamp) AS purchase_date,
+    AVG(JULIANDAY(order_delivered_customer_date) - JULIANDAY(order_purchase_timestamp))
+        AS avg_delivery_time
+FROM orders
+WHERE order_purchase_timestamp >= '2017-06-01' AND order_purchase_timestamp <= '2018-06-30'
+GROUP BY DATE(order_purchase_timestamp)
+"""
+
+review_score_count = """
+SELECT
+    review_score,
+    COUNT(*) AS count
+FROM order_reviews
+GROUP BY review_score
+"""
+
+rfm_buckets = """
+-- Calculate RFM scores
+WITH RecencyScore AS (
+    SELECT customer_unique_id,
+           MAX(order_purchase_timestamp) AS last_purchase,
+           NTILE(5) OVER (ORDER BY MAX(order_purchase_timestamp) DESC) AS recency
+    FROM orders
+        JOIN customers USING (customer_id)
+    WHERE order_status = 'delivered'
+    GROUP BY customer_unique_id
+),
+FrequencyScore AS (
+    SELECT customer_unique_id,
+           COUNT(order_id) AS total_orders,
+           NTILE(5) OVER (ORDER BY COUNT(order_id) DESC) AS frequency
+    FROM orders
+        JOIN customers USING (customer_id)
+    WHERE order_status = 'delivered'
+    GROUP BY customer_unique_id
+),
+MonetaryScore AS (
+    SELECT customer_unique_id,
+           SUM(price) AS total_spent,
+           NTILE(5) OVER (ORDER BY SUM(price) DESC) AS monetary
+    FROM orders
+        JOIN order_items USING (order_id)
+        JOIN customers USING (customer_id)
+    WHERE order_status = 'delivered'
+    GROUP BY customer_unique_id
+),
+-- Assign each customer to a group
+RFM AS (
+    SELECT last_purchase, total_orders, total_spent,
+        CASE
+            WHEN recency = 1 AND frequency + monetary IN (1, 2, 3, 4) THEN "Champions"
+            WHEN recency IN (4, 5) AND frequency + monetary IN (1, 2) THEN "Can't Lose Them"
+            WHEN recency IN (4, 5) AND frequency + monetary IN (3, 4, 5, 6) THEN "Hibernating"
+            WHEN recency IN (4, 5) AND frequency + monetary IN (7, 8, 9, 10) THEN "Lost"
+            WHEN recency IN (2, 3) AND frequency + monetary IN (1, 2, 3, 4) THEN "Loyal Customers"
+            WHEN recency = 3 AND frequency + monetary IN (5, 6) THEN "Needs Attention"
+            WHEN recency = 1 AND frequency + monetary IN (7, 8) THEN "Recent Users"
+            WHEN recency = 1 AND frequency + monetary IN (5, 6) OR
+                recency = 2 AND frequency + monetary IN (5, 6, 7, 8) THEN "Potential Loyalists"
+            WHEN recency = 1 AND frequency + monetary IN (9, 10) THEN "Price Sensitive"
+            WHEN recency = 2 AND frequency + monetary IN (9, 10) THEN "Promising"
+            WHEN recency = 3 AND frequency + monetary IN (7, 8, 9, 10) THEN "About to Sleep"
+        END AS RFM_Bucket
+    FROM RecencyScore
+        JOIN FrequencyScore USING (customer_unique_id)
+        JOIN MonetaryScore USING (customer_unique_id)
+)
+-- Calculate group statistics for plotting
+SELECT RFM_Bucket,
+       AVG(JULIANDAY('now') - JULIANDAY(last_purchase)) AS avg_days_since_purchase,
+       AVG(total_spent / total_orders) AS avg_sales_per_customer,
+       COUNT(*) AS customer_count
+FROM RFM
+GROUP BY RFM_Bucket
+"""
+
+clv = """
+WITH CustomerData AS (
+    SELECT
+        customer_unique_id,
+        customer_zip_code_prefix AS zip_code_prefix,
+        COUNT(DISTINCT orders.order_id) AS order_count,
+        SUM(payment_value) AS total_payment,
+        JULIANDAY(MIN(order_purchase_timestamp)) AS first_order_day,
+        JULIANDAY(MAX(order_purchase_timestamp)) AS last_order_day
+    FROM customers
+        JOIN orders USING (customer_id)
+        JOIN order_payments USING (order_id)
+    GROUP BY customer_unique_id
+)
+SELECT
+    customer_unique_id,
+    zip_code_prefix,
+    order_count AS PF,
+    total_payment / order_count AS AOV,
+    CASE
+        WHEN (last_order_day - first_order_day) < 7 THEN
+            1
+        ELSE
+            (last_order_day - first_order_day) / 7
+        END AS ACL
+FROM CustomerData
+"""
+# Calculate average CLV per zip code prefix and load into DataFrame
+avg_clv_per_zip_prefix = f"""
+WITH CLV AS (
+    {clv}
+)
+SELECT
+    zip_code_prefix AS zip_prefix,
+    AVG(PF * AOV * ACL) AS avg_CLV,
+    COUNT(customer_unique_id) AS customer_count,
+    geolocation_lat AS latitude,
+    geolocation_lng AS longitude
+FROM CLV
+    JOIN geolocation ON CLV.zip_code_prefix = geolocation.geolocation_zip_code_prefix
+GROUP BY zip_code_prefix
+"""
+
