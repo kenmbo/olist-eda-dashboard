53053170e5845dc156a1d66be9f43168ba403ce8
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-04-14 23:01:04 -0500

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+def view_table(table, limit):
+    query = f"""
+        SELECT *
+        FROM {table}
+        LIMIT {limit}
+    """
+    return pd.read_sql_query(query, conn)
+
+def remove_outliers_iqr(df, column, group_column):
+  new_df = pd.DataFrame()
+  for group_value in df[group_column].unique():
+    group_df = df[df[group_column] == group_value]
+    Q1 = group_df[column].quantile(0.25)
+    Q3 = group_df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    filtered_group_df = group_df[(group_df[column] >= lower_bound) & (group_df[column] <= upper_bound)]
+    new_df = pd.concat([new_df, filtered_group_df])
+  return new_df
+
