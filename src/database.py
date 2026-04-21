56fb8fd941759202450696bff1533b2ee137ad80
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-04-20 23:03:56 -0500

    Create get_connectionfunction in src/database.py.

--- src/database.py
+++ src/database.py
@@ -2,3 +2,14 @@ import sqlite3
 import pandas as pd
 import sys
 from src import queries
+
+def get_connection(db_path="../data/olist.sqlite"):
+    """
+    Adjust the db_path depending on where you run the script from.
+    """
+    try:
+        conn = sqlite3.connect(db_path)
+        return conn
+    except sqlite3.Error as e:
+        print(f"Error connecting to database '{db_path}': {e}")
+        sys.exit(1)
