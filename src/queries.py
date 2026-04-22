6e2bb1dab443f1415d3165ddaea74f9735ac47c7
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-04-21 22:35:06 -0500

    Stats for order products and a ranked categories query implemented in src/queries.py.

--- src/queries.py
+++ src/queries.py
@@ -134,6 +134,49 @@ GROUP BY
     category
 """
 
+order_product_and_shipping_costs = """
+SELECT
+    orders.order_id,
+    SUM(price) AS product_cost,
+    SUM(freight_value) AS shipping_cost
+FROM
+    orders
+    JOIN order_items USING (order_id)
+WHERE order_status = 'delivered'
+GROUP BY orders.order_id
+"""
+
+ranked_categories = """
+SELECT
+    product_category_name_english AS category,
+    SUM(price) AS sales,
+    RANK() OVER (ORDER BY SUM(price) DESC) AS rank
+FROM order_items
+    JOIN orders USING (order_id)
+    JOIN products USING (product_id)
+    JOIN product_category_name_translation USING (product_category_name)
+WHERE order_status = 'delivered'
+GROUP BY product_category_name_english
+"""
+
+category_sales_summary = f"""
+WITH RankedCategories AS (
+    {ranked_categories}
+)
+SELECT
+    category,
+    sales
+FROM RankedCategories
+WHERE rank <= 18
+-- Other categories, aggregated
+UNION ALL
+SELECT
+    'Other categories' AS category,
+    SUM(sales) AS sales
+FROM RankedCategories
+WHERE rank > 18
+"""
+
 # ==========================================
 # MISSING QUERIES 
 # ==========================================
