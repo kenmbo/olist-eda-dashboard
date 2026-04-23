cdb4e5f73d17d9f9247d69588d15b66328be6841
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-04-22 23:28:33 -0500

    Implement seller_review_scores_and_sales query in src/queries.py.

--- src/queries.py
+++ src/queries.py
@@ -182,7 +182,20 @@ WHERE rank > 18
 # ==========================================
 
 seller_review_scores_and_sales = """
--- TODO: Insert SQL here
+SELECT
+    sellers.seller_id,
+    AVG(order_reviews.review_score) AS avg_review_score,
+    SUM(order_items.price) AS total_sales,
+    COUNT(orders.order_id) AS num_orders
+FROM
+    sellers
+    LEFT JOIN order_items ON sellers.seller_id = order_items.seller_id
+    LEFT JOIN orders ON order_items.order_id = orders.order_id
+    LEFT JOIN order_reviews ON orders.order_id = order_reviews.order_id
+GROUP BY
+    sellers.seller_id
+HAVING
+    COUNT(orders.order_id) > 10
 """
 
 seller_shipping_times = """
