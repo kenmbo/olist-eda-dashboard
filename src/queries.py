c06429915d0683904604b39e4b1bc50316dd7c2c
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-04-22 23:26:33 -0500

    Implement lead_conversion query in src/queries.

--- src/queries.py
+++ src/queries.py
@@ -190,7 +190,16 @@ seller_shipping_times = """
 """
 
 lead_conversion = """
--- TODO: Insert SQL here
+SELECT
+    COALESCE(origin, 'unknown') AS origin,
+    COUNT(DISTINCT leads_qualified.mql_id) AS qualified_leads,
+    COUNT(DISTINCT leads_closed.mql_id) AS closed_leads,
+    COUNT(DISTINCT leads_closed.mql_id) * 100.0 / COUNT(DISTINCT leads_qualified.mql_id)
+        AS conversion_rate
+FROM leads_qualified
+    LEFT JOIN leads_closed USING (mql_id)
+GROUP BY COALESCE(origin, 'unknown')
+ORDER BY COUNT(leads_qualified.mql_id) DESC
 """
 
 sellers_per_bucket = """
