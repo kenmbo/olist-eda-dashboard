e8280aa1203bdd3cd1f5e4888dba0ef88d472477
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-04-26 02:23:16 -0500

    Add order_stage_times_top_10_citites to queries.py

--- src/queries.py
+++ src/queries.py
@@ -257,6 +257,26 @@ SELECT
 FROM ForecastedSales
 """
 
+order_stage_times_top_10_citites = f"""
+SELECT
+    UPPER(customer_city)
+        AS city,
+    AVG(JULIANDAY(order_approved_at) - JULIANDAY(order_purchase_timestamp))
+        AS approved,
+    AVG(JULIANDAY(order_delivered_carrier_date) - JULIANDAY(order_approved_at))
+        AS delivered_to_carrier,
+    AVG(JULIANDAY(order_delivered_customer_date) - JULIANDAY(order_delivered_carrier_date))
+        AS delivered_to_customer,
+    AVG(JULIANDAY(order_estimated_delivery_date) - JULIANDAY(order_delivered_customer_date))
+        AS estimated_delivery
+FROM orders
+    JOIN customers USING (customer_id)
+WHERE  customer_city IN {tuple(top_cities['customer_city'])}
+GROUP BY  customer_city
+ORDER BY approved + delivered_to_carrier + delivered_to_customer DESC
+"""
+
+
 
 
 # ==========================================
