684e3d30495a78655646d051458e55568a5a9c98
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-04-27 21:32:44 -0500

    Add connnection settings for FastAPI backend.

--- src/main.py
+++ src/main.py
@@ -6,3 +6,15 @@ app = FastAPI(
     title="Olist Dashboard API",
     description="Backend API serving data for the Olist E-commerce Dashboard"
 )
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
