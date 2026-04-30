b41d3f0e9c5d350a04064469197b469569bcbe1d
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-04-29 20:45:12 -0500

    Minor commit: import utils.py into main.py. Utils will be used for the remove_outliers_iqr() function.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src import database 
+from src import database
+from src import utils
 
 app = FastAPI(
     title="Olist Dashboard API",
