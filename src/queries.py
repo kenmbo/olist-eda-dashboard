17bd26ba7c0b59ca5f1241017aaa387f9688ef57
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-07-17 12:31:25 -0500

    feat(queries): add an SQL query to calculate the total sales and average review score per seller

--- src/queries.py
+++ src/queries.py
@@ -488,3 +488,15 @@ WHERE review_score IS NOT NULL
 GROUP BY review_score
 ORDER BY review_score ASC
 """
+
+seller_review_vs_sales = """
+SELECT
+    oi.seller_id,
+    SUM(oi.price) AS total_sales,
+    AVG(r.review_score) AS avg_score,
+    COUNT(DISTINCT oi.order_id) AS order_count
+FROM order_items oi
+JOIN order_reviews r ON oi.order_id = r.order_id
+GROUP BY oi.seller_id
+HAVING order_count > 5
+"""
